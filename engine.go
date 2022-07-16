70f26ebc41239641e84681dce2a4a103936bf68b
Jarod <therealfugu@gmail.com>
2022-07-16 07:40:51 -0600

    ecs refactor

--- engine.go
+++ engine.go
@@ -2,18 +2,15 @@ package engine
 
 import (
 	"fmt"
-	"os"
-	"time"
-
 	"github.com/mjolnir-mud/engine/internal/nats"
 	"github.com/mjolnir-mud/engine/internal/plugin_registry"
-	"github.com/mjolnir-mud/engine/internal/redis"
 	"github.com/mjolnir-mud/engine/pkg/plugin"
 	nats2 "github.com/nats-io/nats.go"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
 )
 
 type engine struct {
@@ -50,7 +47,6 @@ func Start(name string) {
 
 	logger.Info().Str("plugin", "engine").Msgf("initializing engine for game %s", name)
 	nats.Start()
-	redis.Start()
 	plugin_registry.InitPlugins()
 
 	err = e.baseCommand.Execute()
@@ -62,36 +58,11 @@ func Start(name string) {
 
 func Stop() {
 	logger.Info().Str("plugin", "engine").Msg("shutting down engine")
-	redis.Stop()
 	nats.Stop()
 }
 
 func RegisterPlugin(plugin plugin.Plugin) {
 	plugin_registry.Register(plugin)
-}
-
-func RedisSet(key string, value interface{}, expiration time.Duration) error {
-	return redis.Set(key, value, expiration)
-}
-
-func RedisGet(key string, value interface{}) error {
-	return redis.Get(key, value)
-}
-
-func RedisKeyExists(key string) (bool, error) {
-	return redis.KeyExists(key)
-}
-
-func RedisDelete(key string) error {
-	return redis.Delete(key)
-}
-
-func RedisCount(key string) (int64, error) {
-	return redis.Count(key)
-}
-
-func RedisDeleteAll(key string) error {
-	return redis.DeleteAll(key)
 }
 
 func PublishEvent(event string, data interface{}) error {
