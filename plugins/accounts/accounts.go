9e9128d133b49e2d5db17677aea15689f778ed1c
Jarod <therealfugu@gmail.com>
2022-08-10 08:48:56 -0600

    accounts requires ecs

--- plugins/accounts/accounts.go
+++ plugins/accounts/accounts.go
@@ -4,6 +4,7 @@ import (
 	"github.com/mjolnir-mud/engine"
 	"github.com/mjolnir-mud/engine/plugins/accounts/pkg/data_source"
 	"github.com/mjolnir-mud/engine/plugins/data_sources"
+	"github.com/mjolnir-mud/engine/plugins/ecs"
 	"github.com/mjolnir-mud/engine/plugins/mongo_data_source"
 )
 
@@ -16,6 +17,7 @@ func (p *plugin) Name() string {
 func (p *plugin) Registered() error {
 	engine.EnsureRegistered(data_sources.Plugin.Name())
 	engine.EnsureRegistered(mongo_data_source.Plugin.Name())
+	engine.EnsureRegistered(ecs.Plugin.Name())
 
 	data_sources.Register(data_source.DataSource)
 	return nil
