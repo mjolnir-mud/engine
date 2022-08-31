8dca67ee265047a9984937e058841e71c2efaa30
Jarod <therealfugu@gmail.com>
2022-08-31 08:08:45 -0600

    testing helper can start serviceˆ

--- pkg/testing/testing.go
+++ pkg/testing/testing.go
@@ -2,15 +2,22 @@ package testing
 
 import (
 	"github.com/mjolnir-mud/engine"
+	"github.com/mjolnir-mud/engine/internal/instance"
 )
 
-func Setup() chan bool {
+func Setup(services ...string) chan bool {
 	engine.SetEnv("test")
 	ch := make(chan bool)
 	engine.RegisterAfterStartCallback(func() {
 		go func() { ch <- true }()
 	})
 	engine.Start("test")
+
+	if len(services) > 0 {
+		for _, service := range services {
+			instance.StartService(service)
+		}
+	}
 
 	return ch
 }
