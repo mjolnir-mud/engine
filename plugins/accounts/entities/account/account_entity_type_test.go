82d6a951daba1c05261383f081e01781ba5ab5ea
Jarod <therealfugu@gmail.com>
2022-09-27 06:40:46 -0600

    compare credentials

---
+++ plugins/accounts/entities/account/account_entity_type_test.go
@@ -0,0 +1,75 @@
+package account
+
+import (
+	"github.com/mjolnir-mud/engine"
+	engineTesting "github.com/mjolnir-mud/engine/pkg/testing"
+	"github.com/mjolnir-mud/engine/plugins/accounts/data_sources/account"
+	"github.com/mjolnir-mud/engine/plugins/data_sources"
+	dataSourcesTesting "github.com/mjolnir-mud/engine/plugins/data_sources/pkg/testing"
+	"github.com/mjolnir-mud/engine/plugins/ecs"
+	ecsTesting "github.com/mjolnir-mud/engine/plugins/ecs/pkg/testing"
+	mongoDataSourceTesting "github.com/mjolnir-mud/engine/plugins/mongo_data_source/pkg/testing"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
+
+func setup() {
+	engineTesting.Setup("world", func() {
+		dataSourcesTesting.Setup()
+		ecsTesting.Setup()
+		mongoDataSourceTesting.Setup()
+
+		engine.RegisterBeforeServiceStartCallback("world", func() {
+			data_sources.Register(account.Create())
+		})
+
+		engine.RegisterAfterServiceStartCallback("world", func() {
+			ecs.RegisterEntityType(EntityType)
+
+			_ = data_sources.FindAndDelete("accounts", map[string]interface{}{"username": "testaccount"})
+			hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+
+			err := data_sources.SaveWithId(
+				"accounts",
+				"testaccount",
+				map[string]interface{}{
+					"username":       "testaccount",
+					"hashedPassword": string(hashedPassword),
+					"__metadata": map[string]interface{}{
+						"entityType": "account",
+						"collection": "accounts",
+					},
+				})
+
+			if err != nil {
+				panic(err)
+			}
+		})
+	})
+
+}
+
+func teardown() {
+	_ = data_sources.FindAndDelete("accounts", map[string]interface{}{"username": "testaccount"})
+	dataSourcesTesting.Teardown()
+	mongoDataSourceTesting.Teardown()
+	engineTesting.Teardown()
+}
+
+func TestAccountType_Name(t *testing.T) {
+	setup()
+	defer teardown()
+	assert.Equal(t, "account", EntityType.Name())
+}
+
+func TestAccountType_Create(t *testing.T) {
+	assert.Equal(t, map[string]interface{}{"username": "test"}, EntityType.Create(map[string]interface{}{"username": "test"}))
+}
+
+func TestAccountType_Validate(t *testing.T) {
+	setup()
+	defer teardown()
+
+	assert.Error(t, EntityType.Validate(map[string]interface{}{"username": "test"}))
+}
